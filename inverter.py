652ab68a0ec92a25a07034dd70cf68ff3709266a
Khakov Rustam <khakov.rustam@gmail.com>
2018-03-16 14:54:47 +0300

    Fix sort, remove punctuation and formules

--- inverter.py
+++ inverter.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as Elem
 from string import punctuation
+import collections
 
 
 # create index dict for set of words
@@ -18,6 +19,7 @@ def create_index_for_doc(index_dict, words_set, doc_num):
 def write_to_file(filename, index_dict):
     root = Elem.Element("inverter_index")
     words = Elem.SubElement(root, "words")
+    index_dict = collections.OrderedDict(sorted(index_dict.items()))
     for key, value in index_dict.items():
         word = Elem.SubElement(words, "word", attrib={'name': str(key), 'count': str(value['documents_count'])})
         documents = Elem.SubElement(word, "documents")
@@ -36,9 +38,13 @@ def get_index():
     articles = root.find('articles')
     for doc_num, article in enumerate(articles.findall('article')):
         article_mystem_set = set(
-            [w.strip(punctuation) for w in article.find('title_mystem').text.split() + article.find('annotate_mystem').text.split()])
+            [w.strip(punctuation).replace('$', '') for w in
+             article.find('title_mystem').text.split() + article.find('annotate_mystem').text.split()
+             if w.strip()[0] != '$' and w.strip()[-1] != '$'])
         article_porter_set = set(
-            [w.strip(punctuation) for w in (article.find('title_porter').text.split() + article.find('annotate_porter').text.split())])
+            [w.strip(punctuation).replace('$', '') for w in
+             (article.find('title_porter').text.split() + article.find('annotate_porter').text.split())
+             if w.strip()[0] != '$' and w.strip()[-1] != '$'])
         create_index_for_doc(porter_index, article_porter_set, doc_num)
         create_index_for_doc(mystem_index, article_mystem_set, doc_num)
     write_to_file('index_mystem.xml', mystem_index)
